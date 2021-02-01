2e3f1093b87d62cb0ee6099249c2b27dd92529ff
Lev Michael <lev.choubine@gmail.com>
2021-02-01 17:25:08 -0500

    fonts

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,21 +1,33 @@
-import Nav from './Nav'
+
 import Contact from './Contact'
+import { Link } from 'react-router-dom'
 
 function Photo() {
+  function showProjects(){
+    document.querySelector('#projects').style.display="block"
+  }
 
+    function hideProjects(){
+      document.querySelector('#projects').style.display="none"
+    }
   
       return(
      
         <div id="midHeader">
       <Contact /> 
         <div id="photoBox">
-        <h5 ><a href="https://github.com/lev-choubine"><img className="gittohabbu" src="https://image.flaticon.com/icons/png/512/25/25231.png"/></a></h5>
-        <img id="pic" src='https://avatars0.githubusercontent.com/u/68445801?v=4'/>
-         <h5><a href="https://www.linkedin.com/in/lev-choubine/"><img className="gittohabbu" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/1200px-Linkedin_icon.svg.png"/></a></h5>
-        
+        <div className="photoButtons" > <a href="https://github.com/lev-choubine"><img className="gittohabbu" src="https://image.flaticon.com/icons/png/512/25/25231.png"/></a>
+        <li class="navLink" onClick={showProjects}><Link to="/">PROJECTS</Link></li>
         </div>
-        <h6>Full stack developer. <br/>Graduated from General Assembly's Software Engineering Immersive program.</h6>
-        <Nav />
+        <img id="pic" src='https://avatars0.githubusercontent.com/u/68445801?v=4'/>
+         <div className="photoButtons">
+         <li class="navLink" onClick={hideProjects}><Link to="/Resume">RESUME</Link></li>
+        
+           <a href="https://www.linkedin.com/in/lev-choubine/"><img className="gittohabbu" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/1200px-Linkedin_icon.svg.png"/></a>
+        
+           </div>
+        </div>
+        <h6 id="speil">Full stack developer. <br/>Graduated from General Assembly's Software Engineering Immersive program.</h6>
       </div>
     
     );
