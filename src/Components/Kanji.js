649478f7b40e64ce66274b35c4f7bac22abe09ce
Lev Michael <lev.choubine@gmail.com>
2021-02-16 14:07:32 -0500

    better styling an resizing kanji

--- src/Components/Kanji.js
+++ src/Components/Kanji.js
@@ -69,22 +69,28 @@ function Kanji() {
             </h4>
             <div id="kanjiApp">
             {mode==="kanji"?
-            <div>
+            <div id="upperBody">
+                <div>
                 <p id="kanji">
                     {kanjiResult ? 
                         kanjiResult[0].slug
                         : ""}
                 </p>
-                <p>reading: </p>
+                </div>
+                <div>
                 <p id="reading">
+              
+                <p>reading: </p>    
                 {kanjiResult ? 
                     kanjiResult[0].japanese[0].reading                    
                     : ""}
                 </p>
+                </div>
             </div>
             :
             <p id="english">  {kanjiResult ? kanjiResult[0].senses[0].english_definitions[0]: "" }</p>
             }
+            <div id="lowerBody">
             <form id="searchBar" onSubmit={e=>{e.target.reset(); e.preventDefault(); setKanjiLookUp('')}}>
                 <div id="fakeinput">
                     <p >{kanjiLookUp}</p>
@@ -103,6 +109,7 @@ function Kanji() {
                 <label>Get Englsih</label>
                 <input type="radio" name="kanji" value="english" onClick={getEnglish} ></input>
                 </form>
+            </div>
             </div>
             </div>
         </div>
